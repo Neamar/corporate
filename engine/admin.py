7b536468328006d834d689d5f457700015595f25
Hugo Duroux <hugoduroux@free.fr>
2013-12-26 14:57:30 +0100

    add engine's admin

--- engine/admin.py
+++ engine/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from engine.models import Game, Message, Player, Order
 
-# Register your models here.
+class GameAdmin(admin.ModelAdmin):
+	list_display = ('current_turn', 'total_turn')
+
+class PlayerAdmin(admin.ModelAdmin):
+	list_display = ('user', 'name', 'game')
+
+class MessageAdmin(admin.ModelAdmin):
+	list_display = ('author', 'title', 'public', 'player', 'public')
+
+class OrderAdmin(admin.ModelAdmin):
+	list_display = ('player', 'game', 'turn')
