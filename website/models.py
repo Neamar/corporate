ed8f05407dfaf8bf729221d31417a5c67e7e54a3
Neamar <neamar@neamar.fr>
2013-12-26 14:06:50 +0100

    Custom user model

--- website/models.py
+++ website/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
 
-# Create your models here.
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=254, unique=True)
+    phone = models.CharField(max_length=15, null=True, blank=True)
+
+    USERNAME_FIELD = 'email'
