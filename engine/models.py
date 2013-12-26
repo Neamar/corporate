4c431ecbc8c265c40ddc1a583ed61d8dc9cb5f55
Neamar <neamar@neamar.fr>
2013-12-26 16:57:58 +0100

    Added city to game

--- engine/models.py
+++ engine/models.py
@@ -3,14 +3,23 @@ from datetime import datetime
 from django.conf import settings
 
 class Game(models.Model):
+	city = models.CharField(max_length=50)
 	current_turn = models.PositiveSmallIntegerField(default=1)
 	total_turn = models.PositiveSmallIntegerField()
 	started = models.DateTimeField(default=datetime.now)
 
+	def __unicode__(self):
+		return "Corporate Game: %s" % self.city
+
+
 class Player(models.Model):
+	name = models.CharField(max_length=64)
 	user = models.ForeignKey(settings.AUTH_USER_MODEL)
 	game = models.ForeignKey(Game)
-	name = models.CharField(max_length=64)
+
+
+	def __unicode__(self):
+		return self.name
 
 
 class Message(models.Model):
@@ -19,6 +28,7 @@ class Message(models.Model):
 	author = models.ForeignKey(Player)
 	public = models.BooleanField(default=False)
 	recipient_set = models.ManyToManyField('Player', related_name="+")
+
 
 class Order(models.Model):
 	player = models.ForeignKey(Player)
