431458bd3f85febf2f109996e8b491ed93692276
Neamar <neamar@neamar.fr>
2013-12-26 14:30:44 +0100

    User admin

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from website.models import User
+
+admin.site.register(User, UserAdmin)
