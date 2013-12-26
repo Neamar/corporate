96be3f0eb133d33cbcb43161fa2ab76b16d7b4eb
Neamar <neamar@neamar.fr>
2013-12-26 14:23:19 +0100

    Custom user model

--- website/models.py
+++ website/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager
 
 class User(AbstractBaseUser):
-    email = models.EmailField(max_length=254, unique=True)
-    phone = models.CharField(max_length=15, null=True, blank=True)
-
-    USERNAME_FIELD = 'email'
+	objects = UserManager()
+	
+	email = models.EmailField(max_length=254, unique=True)
+	phone = models.CharField(max_length=15, null=True, blank=True)
+	# TODO: add image
+	 
+	USERNAME_FIELD = 'email'
