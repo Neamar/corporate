89350f3cbfa21f9767e1c5e9ef2e8e886a9096ef
Hugo Duroux <hugoduroux@free.fr>
2013-12-26 14:39:39 +0100

    update engine's models

--- engine/models.py
+++ engine/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import User
 
 class Game(models.Model):
 	current_turn = models.PositiveIntegerField(default=1)
-	total_turn = models.PositiveIntegerField(default=1)
+	total_turn = models.PositiveIntegerField()
 
 
 class Player(NamedModel):
-	user = models.OneToOneField(User, null=True)
-	game = models.ForeignKey(Game, null=True)
+	user = models.OneToManyField(User)
+	game = models.ForeignKey(Game)
 
 
 class Message(models.Model):
 	title = models.CharField(max_length=256, blank=True)
 	content = models.TextField(blank=True)
-	author = models.Foreignkey(Player, null=True)
+	author = models.Foreignkey(Player)
 	public = models.BooleanField(default=False)
-
+	recipient = ManyToManyField('Player')
 
 class Order(models.Model):
-	player = models.Foreignkey(Player, null=True)
-	game = models.ForeignKey(Game, null=True)
-	turn = models.PositiveIntegerField(default=1)
+	player = models.Foreignkey(Player)
+	game = models.ForeignKey(Game)
+	turn = models.PositiveIntegerField()
