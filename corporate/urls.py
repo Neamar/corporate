13a17a0da184024694105615c86c53bbb9dba0dd
Neamar <neamar@neamar.fr>
2013-12-26 14:48:39 +0100

    Index page

--- corporate/urls.py
+++ corporate/urls.py
@@ -4,9 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	# Examples:
-	# url(r'^$', 'corporate.views.home', name='home'),
-	# url(r'^blog/', include('blog.urls')),
-	url(r'^login/$', 'django.contrib.auth.views.login'),
 	url(r'^admin/', include(admin.site.urls)),
+	#url(r'game/', include('engine.urls')),
+	url(r'', include('website.urls')),
 	)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required
+def index(request):
+	return render(request, 'index.html', {})
