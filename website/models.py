d574b6e2b978d4e8d4bbd1b9a447f5754ae33877
Neamar <neamar@neamar.fr>
2013-12-26 14:28:47 +0100

    Extend BaseUser

--- website/models.py
+++ website/models.py
@@ -1,12 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
 
-class User(AbstractBaseUser):
+class User(AbstractUser):
 	objects = UserManager()
-	
-	email = models.EmailField(max_length=254, unique=True)
+
 	phone = models.CharField(max_length=15, null=True, blank=True)
 	# TODO: add image
-	 
-	USERNAME_FIELD = 'email'
