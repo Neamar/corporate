b54fb78d09a4a4df7c5b2da83c77b48e271c4cbe
Neamar <neamar@neamar.fr>
2013-12-26 15:10:52 +0100

    Register module code

--- engine/modules.py
+++ engine/modules.py
@@ -1,6 +1,24 @@
 
 
-custom_urls = {}
+"""
+List of task builder to call before each game resolution
+"""
+tasks_list = []
+
+"""
+List of available orders
+"""
+orders_list = []
+
+"""
+List of function to call to setup a new game
+"""
+setups_list = []
+
+"""
+List of custom urls
+"""
+views_list = {}
 
 class ResolutionTask:
 	"""
@@ -12,7 +30,7 @@ class ResolutionTask:
 	task = lambda game: game
 
 
-def registerModule(taskBuilder, orders, views, setup):
+def registerModule(task_builder=None, orders=[], views={}, setup=None):
 	"""
 	Register a new module for use in the game.
 	taskBuilder is a function to retrieve a list of tasks items for resolution
@@ -20,3 +38,15 @@ def registerModule(taskBuilder, orders, views, setup):
 	views is a dict whose keys are regexp and values associated functions. If a conflict occurs between multiple apps, the last entry prevails.
 	setup is a function to call on game initialisation
 	"""
+	global tasks_list, orders_list, setups_list, views_list
+	
+	if task_builder:
+		tasks_list.append(task_builder)
+
+	if setup:
+		setups_list.append(setup)
+
+	orders_list += orders
+	views_list.update(views)
+
+
