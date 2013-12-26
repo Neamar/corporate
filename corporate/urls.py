e1cbdf80fc106f10a0acf2e9caefcfc4fbca0d0d
Neamar <neamar@neamar.fr>
2013-12-26 14:23:23 +0100

    Login view

--- corporate/urls.py
+++ corporate/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'corporate.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-)
+	# Examples:
+	# url(r'^$', 'corporate.views.home', name='home'),
+	# url(r'^blog/', include('blog.urls')),
+	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^admin/', include(admin.site.urls)),
+	)
