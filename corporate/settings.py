a54dcf67b909c5c3f1d9987519bd27e22c1f309a
Neamar <neamar@neamar.fr>
2013-12-26 15:37:39 +0100

    Setup influence model

--- corporate/settings.py
+++ corporate/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'website',
     'engine',
+    'engine_modules.influence',
 )
 
 MIDDLEWARE_CLASSES = (
--- engine_modules/influence/models.py
+++ engine_modules/influence/models.py
@@ -2,6 +2,6 @@ from django.db import models
 from engine.models import Player
 
 
-class PlayerInfluence(models.model):
+class PlayerInfluence(models.Model):
 	player = models.OneToOneField(Player)
 	level = models.PositiveSmallIntegerField()
