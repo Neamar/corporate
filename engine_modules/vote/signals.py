215994a6f510c94e5fa423b7640158768fcf1dcb
Hugo Duroux <hugoduroux@free.fr>
2013-12-27 19:20:18 +0100

    add influence in votes

---
+++ engine_modules/vote/signals.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from django.dispatch import receiver
+
+from engine.dispatchs import validate_order
+from engine.exceptions import OrderNotAvailable
+from engine_modules.vote.orders import VoteOrder
+
+@receiver(validate_order, sender=VoteOrder)
+def limit_buy_share_by_influence(sender, instance, **kwargs):
+	"""
+	You can't vote more than {{influence}} share per turn
+	"""
+	if VoteOrder.objects.filter(player=instance.player, turn=instance.player.game.current_turn).count() >= instance.player.influence.level:
+		raise OrderNotAvailable("Pas assez d'influence pour voter à nouveau ce tour-ci.")
--- engine_modules/vote/tests/test_orders.py
+++ engine_modules/vote/tests/test_orders.py
@@ -1,4 +1,5 @@
 from engine.testcases import EngineTestCase
+from engine.exceptions import OrderNotAvailable
 from engine_modules.corporation.models import BaseCorporation, Corporation
 from engine_modules.vote.orders import VoteOrder
 
@@ -33,3 +34,38 @@ class OrdersTest(EngineTestCase):
 
 		self.assertEqual(self.reload(c).assets, 11)
 		self.assertEqual(self.reload(c2).assets, 14)
+
+	def test_cant_vote_more_than_influence(self):
+		self.p.influence.level=1
+		self.p.save()
+
+		c = Corporation.objects.get(base_corporation=self.bc)
+		c.assets = 10
+		c.save()
+
+		c2 = Corporation.objects.get(base_corporation=self.bc2)
+		c2.assets = 15
+		c2.save()
+
+		o = VoteOrder(
+			corporation_up=c, 
+			corporation_down=c2, 
+			player=self.p
+		)
+		# assertNoRaises
+		o.save()
+		
+
+		o2 = VoteOrder(
+			corporation_up=c, 
+			corporation_down=c2, 
+			player=self.p
+		)
+		
+		self.assertRaises(OrderNotAvailable, o2.clean)
+
+		self.p.influence.level=2
+		self.p.save()
+
+		# assertNoRaises
+		o.clean()
