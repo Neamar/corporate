127f82b5aa5da60e53ed842decdfcc7b3701072b
Neamar <neamar@neamar.fr>
2013-12-27 12:05:27 +0100

    Influence is incremented

--- engine_modules/influence/tests/test_orders.py
+++ engine_modules/influence/tests/test_orders.py
@@ -10,9 +10,10 @@ class OrdersTest(TestCase):
 	Unit tests for engine models
 	"""
 	def setUp(self):
+		self.initial_money = 1000000
 		self.g = Game(total_turn=10)
 		self.g.save()
-		self.p = Player(game=self.g)
+		self.p = Player(game=self.g, money=self.initial_money)
 		self.p.save()
 		self.o = BuyInfluenceOrder(
 			player=self.p
@@ -23,12 +24,14 @@ class OrdersTest(TestCase):
 		"""
 		The new player should have influence of 1
 		"""
-		initial_money = 1000000
-		self.p.money = initial_money
-		self.p.save()
-		self.p.influence.level = 1
-		self.p.influence.save()
-
 		self.g.resolve_current_turn()
 
-		self.assertEqual(Player.objects.get(pk=self.p.pk).money, initial_money - BuyInfluenceOrder.BASE_COST * 2)
+		self.assertEqual(Player.objects.get(pk=self.p.pk).money, self.initial_money - BuyInfluenceOrder.BASE_COST * 2)
+
+	def test_order_increment_influence(self):
+		"""
+		The new player should have influence of 1
+		"""
+		self.g.resolve_current_turn()
+
+		self.assertEqual(Player.objects.get(pk=self.p.pk).influence.level, 2)
