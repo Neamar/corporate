f6594142793666f31cdd4603853612bdf6c4c4b3
Hugo Duroux <hugoduroux@free.fr>
2013-12-27 14:40:59 +0100

    update invisible hand task

--- engine_modules/invisible_hand/tasks.py
+++ engine_modules/invisible_hand/tasks.py
@@ -7,20 +7,13 @@ class InvisibleHandTask(ResolutionTask):
 	priority = 40
 
 	def run(self, game):
-		if game.corporation_set.count() > 1:
-			corpo_up = game.corporation_set.all().order_by['?'][0]
-			corpo_down = game.corporation_set.all()
-			corpo_down = corpo_down.exclude(pk=corpo_up.pk).order_by['?'][0]
+		corpos = game.corporation_set.all().order_by['?'][0:2]
+		corpos[0].assets += 1
+		if len(corpos) >= 2:
+			corpos[1] -= 1
+		[corpo.save() for corpo in corpos]
 
-			corpo_up.assets += 1
-			corpo_up.save()
 
-			corpo_down.assets -= 1
-			corpo_down.save()
-		else :
-			corpo_up = game.corporation_set.all()[0]
-			corpo_up.assets += 1
-			corpo_up.save()
 
 tasks = (InvisibleHandTask,)		
 
