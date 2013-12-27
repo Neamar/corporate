289c3e5d82a713122571265ffc791c730b105dd3
Neamar <neamar@neamar.fr>
2013-12-27 12:42:17 +0100

    Post_create signal

--- engine_modules/corporation/signals.py
+++ engine_modules/corporation/signals.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from engine.dispatchs import post_create
 
 from engine.models import Game
 from engine_modules.corporation.models import BaseCorporation, Corporation
 
 
-@receiver(post_save, sender=Game)
-def auto_create_corporation(sender, instance, created, **kwargs):
+@receiver(post_create, sender=Game)
+def auto_create_corporation(sender, instance, **kwargs):
 	"""
 	Create influence model for new player
 	"""
-	if created:
-		base_corporations = BaseCorporation.objects.all()
-		for base_corporation in base_corporations:
-			Corporation(
-				base_corporation=base_corporation,
-				game=instance,
-				assets=10
-			).save()
+	base_corporations = BaseCorporation.objects.all()
+	for base_corporation in base_corporations:
+		Corporation(
+			base_corporation=base_corporation,
+			game=instance,
+			assets=10
+		).save()
