158d368aa82e197d6e60ff7d600cc498cd6e2fea
Neamar <neamar@neamar.fr>
2013-12-28 12:22:06 +0100

    Improved share tests

--- engine_modules/share/tests/test_tasks.py
+++ engine_modules/share/tests/test_tasks.py
@@ -12,11 +12,22 @@ class TasksTest(EngineTestCase):
 	def setUp(self):
 		self.bc = BaseCorporation(name="NC&T", description="Reckless")
 		self.bc.save()
+		self.bc2 = BaseCorporation(name="Renraku", description="Priceless")
+		self.bc2.save()
+		self.bc3 = BaseCorporation(name="Ares", description="Ruthless")
+		self.bc3.save()
 
 		super(TasksTest, self).setUp()
 
 		self.c = self.g.corporation_set.get(base_corporation=self.bc)
+		self.c.assets -= 2
+		self.c.save()
+
+		self.c2 = self.g.corporation_set.get(base_corporation=self.bc2)
 		
+		self.c3 = self.g.corporation_set.get(base_corporation=self.bc3)
+		self.c3.assets += 2
+		self.c3.save()
 
 	def test_buy_task_applied(self):
 		"""
@@ -36,9 +47,50 @@ class TasksTest(EngineTestCase):
 		self.assertEqual(Share.objects.get(pk=1).turn, self.g.current_turn - 1)
 
 
-	def test_dividend_task_applied(self):
+	def test_dividend_task_applied_medium_corporation(self):
 		"""
 		The player should get dividend for previous shares
+		"""
+		# We have one share
+		self.s = Share(
+			player=self.p,
+			corporation=self.c2
+		)
+		self.s.save()
+		self.g.current_turn = 5
+		self.g.save()
+
+		self.g.resolve_current_turn()
+
+		# We expect dividend on this share
+		expected = self.initial_money + DividendTask.SHARE_BASE_VALUE * self.reload(self.c2).assets
+
+		self.assertEqual(self.reload(self.p).money, int(expected))
+
+	def test_dividend_task_applied_first_corporation(self):
+		"""
+		The player should get dividend for previous shares, with bonus if corporation is first
+		"""
+		# We have one share
+		self.s = Share(
+			player=self.p,
+			corporation=self.c3
+		)
+		self.s.save()
+
+		self.g.current_turn = 5
+		self.g.save()
+
+		self.g.resolve_current_turn()
+		# We expect dividend on this share, taking into account the fact that this corporation is the first.
+		expected = self.initial_money + DividendTask.SHARE_BASE_VALUE * self.reload(self.c3).assets * DividendTask.FIRST_BONUS
+
+		self.assertEqual(self.reload(self.p).money, int(expected))
+
+
+	def test_dividend_task_applied_last_corporation(self):
+		"""
+		The player should get dividend for previous shares, with malus if corporation is last
 		"""
 		# We have one share
 		self.s = Share(
@@ -51,7 +103,7 @@ class TasksTest(EngineTestCase):
 		self.g.save()
 
 		self.g.resolve_current_turn()
-		# We expect dividend on this share, taking into account the fact that this corporation is both first and last.
-		expected = self.initial_money + DividendTask.SHARE_BASE_VALUE * self.reload(self.c).assets * 1.25 * 0.75
+		# We expect dividend on this share, taking into account the fact that this corporation is the last.
+		expected = self.initial_money + DividendTask.SHARE_BASE_VALUE * self.reload(self.c).assets * DividendTask.LAST_BONUS
 
 		self.assertEqual(self.reload(self.p).money, int(expected))
