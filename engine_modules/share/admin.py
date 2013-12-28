9eef31e6da75a4dfa4f1db73c13220c2fd9f1246
Neamar <neamar@neamar.fr>
2013-12-28 12:01:48 +0100

    BuyShareAdmin

--- engine_modules/share/admin.py
+++ engine_modules/share/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 
 from engine_modules.share.models import Share
+from engine_modules.share.orders import BuyShareOrder
 
 
 class ShareAdmin(admin.ModelAdmin):
 	list_display = ('corporation', 'player', 'turn')
 	readonly_fields = ('turn',)
 admin.site.register(Share, ShareAdmin)
+
+
+class BuyShareAdmin(admin.ModelAdmin):
+	list_display = ('player', 'corporation', 'turn')
+admin.site.register(BuyShareOrder, BuyShareAdmin)
