42ca39bb09a88d6292aff328d0a6ca618e875355
Neamar <neamar@neamar.fr>
2013-12-28 12:41:01 +0100

    Typos

--- engine_modules/citizenship/signals.py
+++ engine_modules/citizenship/signals.py
@@ -10,16 +10,16 @@ from engine.models import Player
 
 
 @receiver(post_create, sender=Player)
-def auto_create_player_influence(sender, instance, **kwargs):
+def auto_create_player_citizenship(sender, instance, **kwargs):
 	"""
-	Create influence model for new player
+	Create citizenship model for new player
 	"""
 	CitizenShip(player=instance).save()
 
 @receiver(validate_order, sender=CitizenShipOrder)
-def limit_buy_share_by_influence(sender, instance, **kwargs):
+def limit_citizenship_order(sender, instance, **kwargs):
 	"""
-	Can't change citizenship 2 times at the same turn
+	Can't change citizenship 2 times in the same turn
 	"""
 	if CitizenShipOrder.objects.filter(player=instance.player, turn=instance.player.game.current_turn).exists():
 		raise OrderNotAvailable("Vous ne pouvez pas demander une citoyenneté deux fois dans le même tour.")
--- engine_modules/citizenship/tasks.py
+++ engine_modules/citizenship/tasks.py
@@ -3,13 +3,13 @@ from engine_modules.citizenship.orders import CitizenShipOrder
 
 class CitizenshipTask(ResolutionTask):
 	"""
-	Add a citizenship to a player
+	Update player citizenship
 	"""
 	priority = 90
 
 	def run(self, game):
 		"""
-		Retrieve all BuyInfluenceOrder and resolve them
+		Retrieve all CitizenShipOrder and resolve them
 		"""
 		citizen_ship_orders = CitizenShipOrder.objects.filter(player__game=game, turn=game.current_turn)
 		for citizen_ship_order in citizen_ship_orders:
