5c16405160491eedc2d727a3b1bfa6b17924ed00
your-mom-noob <bom@neamar.fr>
2014-01-05 15:53:37 +0100

    Not working push for debugging help

--- engine_modules/corporation_run/models.py
+++ engine_modules/corporation_run/models.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.core.validators import MaxValueValidator
-from random import randint
+from random import randint, shuffle
 
 from engine.models import Order
 from engine_modules.run.models import RunOrder
@@ -11,22 +11,45 @@ class DataStealOrder(RunOrder):
 	"""
 	Model for DataSteal Runs
 	"""
+	has_succeeded = False
 	stolen_corporation = models.ForeignKey(Corporation, related_name="+")	
 	stealer_corporation = models.ForeignKey(Corporation, related_name="+")	
 
+		
+	
 	def resolve_successful(self):
 
-		self.stealer_corporation.assets += 1
-		self.stealer_corporation.save()
-	
-		#Send a note for final message 
-		title=u"Run de Datasteal"
-		content=u"Votre équipe a réussi à voler des données de %s pour le compte de %s" %(self.stolen_corporation.base_corporation.name, self.stealer_corporation.base_corporation.name)
-		self.player.add_note(title=title, content=content)
+		protected = shuffle(self.stolen_corporation.protecteurs.filter(done=False))
+		protected = shuffle(self.stolen_corporation.protecteurs.all())
+		print "Protected : "
+		print protected
+
+		if protected != None:
+			po = protected[0]
+			po.done = True
+			po.save()
+
+			# Send a note to the one who ordered the DataSteal
+			title=u"Run de Datasteal"
+			content=u"Votre équipe a été inerceptée par une autre lors de la tentative de DataSteal sur %s. Elle a cependant réussi à s'enfuir" %(self.stolen_corporation.base_corporation.name)
+			self.player.add_note(title=title, content=content)
+
+			# Send a note to the one who ordered the Protection
+			title=u"Run de Protection"
+			content=u"Votre équipe a réussi à protéger %s d'une tentative de DataSteal. L'équipe adverse a cependant réussi à s'enfuir" %(po.protected_corporation)
+			po.player.add_note(title=title, content=content)
+		else:
+			self.stealer_corporation.assets += 1
+			self.stealer_corporation.save()
+
+			# Send a note for final message 
+			title=u"Run de Datasteal"
+			content=u"Votre équipe a réussi à voler des données de %s pour le compte de %s" %(self.stolen_corporation.base_corporation.name, self.stealer_corporation.base_corporation.name)
+			self.player.add_note(title=title, content=content)
 
 	def resolve_failure(self):
 		
-		#Send a note for final message
+		# Send a note for final message
 		title=u"Run de Datasteal"
 		content=u"La tentative de vol de données de votre équipe sur %s pour le compte de %s a échoué" %(self.stolen_corporation.base_corporation.name, self.stealer_corporation.base_corporation.name)
 		self.player.add_note(title=title, content=content)
@@ -34,10 +57,10 @@ class DataStealOrder(RunOrder):
 	# The one who ordered the run should probably be made aware if his team got captured
 	def resolve_capture(self):
 
-		#Send a note for final message
+		# Send a note for final message
 		title=u"Capture"
-		content=u"L'équipe que vous aviez envoyée pour voler des données à %s a été capturée"
-		self.player.add_note(title=title, content=content) %(self.stolen_corporation.base_corporation.name)
+		content=u"L'équipe que vous aviez envoyée pour voler des données à %s a été capturée" %(self.stolen_corporation.base_corporation.name)
+		self.player.add_note(title=title, content=content)
 		
 	def description(self):
 		return u"Envoyer une équipe voler des données de %s pour le compte de %s" %(self.stolen_corporation.base_corporation.name, self.stealer_corporation.base_corporation.name)
@@ -46,12 +69,25 @@ class ProtectionOrder(RunOrder):
 	"""
 	Model for Protection Runs
 	"""
-	protected_corporation = models.ForeignKey(Corporation, related_name="+")
+	protected_corporation = models.ForeignKey(Corporation, related_name="protecteurs")
+	done = models.BooleanField(default=False)
 
 	def resolve_successful(self):
-		pass
+		self.done = False
+		self.save()
+
 	def resolve_failure(self):
-		pass
+
+		# This should enable the system to keep track of failed Protection Runs
+		# and still distinguish between failed and successful Runs 
+		# It will not, however, let you distinguish them after the turn has ended
+		self.done = True
+
+		# Send a note for final message
+		title=u"run de Protection"
+		content=u"L'équipe que vous aviez envoyée protéger %s a échoué" %(self.protected_corporation.base_corporation.name)
+		self.player.add_note(title=title, content=content)
+
 	def description(self):
 		return u"Envoyer une équipe protéger les intérêts de %s" %(self.protected_corporation.base_corporation.name)
 
@@ -63,26 +99,47 @@ class SabotageOrder(RunOrder):
 
 	def resolve_successful(self):
 
-		self.sabotaged_corporation.assets -= 2
-		self.sabotaged_corporation.save()
+		protected = shuffle(self.sabotaged_corporation.protecteurs.filter(done=False))
+		protected = shuffle(self.sabotaged_corporation.protecteurs.all())
+		print "Sabotage Protected : "
+		print protected
 
-		#Send a note for final message 
-		title=u"Run de Sabotage"
-		content=u"Votre équipe a réussi à saboter les opérations de %s" %(self.sabotaged_corporation.base_corporation.name)
-		self.player.add_note(title=title, content=content)
+		if protected != None:
+			po = protected[0]
+			po.done = True
+			po.save()
+
+			# Send a note to the one who ordered the DataSteal
+			title=u"Run de Sabotage"
+			content=u"Votre équipe a été inerceptée par une autre lors de la tentative de Sabotage sur %s. Elle a cependant réussi à s'enfuir" %(self.sabotaged_corporation.base_corporation.name)
+			self.player.add_note(title=title, content=content)
+
+			# Send a note to the one who ordered the Protection
+			title=u"Run de Protection"
+			content=u"Votre équipe a réussi à protéger %s d'une tentative de DataSteal. L'équipe adverse a cependant réussi à s'enfuir" %(po.protected_corporation)
+			po.player.add_note(title=title, content=content)
+		else:
+			self.sabotaged_corporation.assets -= 2
+			self.sabotaged_corporation.save()
+
+			# Send a note for final message 
+			title=u"Run de Sabotage"
+			content=u"Votre équipe a réussi à saboter les opérations de %s" %(self.sabotaged_corporation.base_corporation.name)
+			self.player.add_note(title=title, content=content)
 
 	def resolve_failure(self):
 
-		#Send a note for final message 
+		# Send a note for final message 
 		title=u"Run de Sabotage"
 		content=u"La tentative de votre équipe pour saboter %s a échoué" %(self.sabotaged_corporation.base_corporation.name)
 		self.player.add_note(title=title, content=content)
-	"""
-	The one who ordered the run should probably be made aware if his team got captured
-	"""
-	def resolve_capture(self):
 
-		#Send a note for final message
+	def resolve_capture(self):
+		"""
+		The one who ordered the run should probably be made aware if his team got captured
+		"""
+
+		# Send a note for final message
 		title=u"Capture"
 		content=u"L'équipe que vous aviez envoyée saboter %s a été capturée" %(self.sabotaged_corporation.base_corporation.name)
 		self.player.add_note(title=title, content=content)
--- engine_modules/corporation_run/tests/test_orders.py
+++ engine_modules/corporation_run/tests/test_orders.py
@@ -2,15 +2,22 @@ from engine.testcases import EngineTestCase
 from engine_modules.corporation_run.models import DataStealOrder, ProtectionOrder, SabotageOrder
 from engine.exceptions import OrderNotAvailable
 from engine_modules.vote.tests.test_orders import OrdersTest
+from engine_modules.corporation.models import BaseCorporation, Corporation
 
 class RunOrdersTest(OrdersTest):
 	def setUp(self):
 		super(RunOrdersTest, self).setUp()
+
+		self.c.protected = None
+		self.c.save()
 		
+		self.c2.protected = None
+		self.c2.save()
+
 		self.dso = DataStealOrder(
 			player=self.p,
-			stealer_corporation=self.c,
-			stolen_corporation=self.c2
+			stealer_corporation=self.c2,
+			stolen_corporation=self.c
 		)
 		self.dso.clean()
 		self.dso.save()
@@ -28,18 +35,20 @@ class RunOrdersTest(OrdersTest):
 		)
 		self.so.clean()
 		self.so.save()
-	"""
-	Datasteal benefits the stealer 1 asset without costing the stolen
-	"""
+
 	def test_datasteal_success(self):
+		"""
+		Datasteal benefits the stealer 1 asset without costing the stolen
+		"""
 
 		begin_assets_stealer = self.dso.stealer_corporation.assets
 		begin_assets_stolen = self.dso.stolen_corporation.assets
 
 		self.dso.resolve_successful()
 
-		self.assertEqual(self.reload(self.dso).stealer_corporation.assets, begin_assets_stealer + 1)
-		self.assertEqual(self.reload(self.dso).stolen_corporation.assets, begin_assets_stolen)		
+		self.assertEqual(self.reload(self.dso.stealer_corporation).assets, begin_assets_stealer + 1)
+		self.assertEqual(self.reload(self.dso.stolen_corporation).assets, begin_assets_stolen)		
+
 	def test_datasteal_failure(self):
 
 		begin_assets_stealer = self.dso.stealer_corporation.assets
@@ -47,22 +56,99 @@ class RunOrdersTest(OrdersTest):
 
 		self.dso.resolve_failure()
 
-		self.assertEqual(self.reload(self.dso).stealer_corporation.assets, begin_assets_stealer)
-		self.assertEqual(self.reload(self.dso).stolen_corporation.assets, begin_assets_stolen)		
-	"""
-	Sabotage doesn't benefit anyone, but costs the sabotaged 2 assets
-	"""
+		self.assertEqual(self.reload(self.dso.stealer_corporation).assets, begin_assets_stealer)
+		self.assertEqual(self.reload(self.dso.stolen_corporation).assets, begin_assets_stolen)		
+
 	def test_sabotage_successful(self):
+		"""
+		Sabotage doesn't benefit anyone, but costs the sabotaged 2 assets
+		"""
 
 		begin_assets = self.so.sabotaged_corporation.assets
 
 		self.so.resolve_successful()
 
-		self.assertEqual(self.reload(self.so).sabotaged_corporation.assets, begin_assets - 2)
+		self.assertEqual(self.reload(self.so.sabotaged_corporation).assets, begin_assets - 2)
 
 	def test_sabotage_failure(self):
 
 		begin_assets = self.so.sabotaged_corporation.assets
 
 		self.so.resolve_failure()
-		self.assertEqual(self.reload(self.so).sabotaged_corporation.assets, begin_assets)
+		self.assertEqual(self.reload(self.so.sabotaged_corporation).assets, begin_assets)
+
+	def test_multiple_datasteal(self):
+		"""
+		Only the first successful DataSteal on a same corporation can benefit someone
+		The others succeed, but the clients do not profit from them
+		"""
+
+		begin_assets_stealer = self.dso.stealer_corporation.assets
+		begin_assets_stolen = self.dso.stolen_corporation.assets
+
+		self.dso.resolve_successful()
+		self.dso.clean()
+
+		self.dso.resolve_successful()	
+		self.assertEqual(self.reload(self.dso.stealer_corporation).assets, begin_assets_stealer + 1)
+	
+	def test_so_po(self):
+		"""
+		Test that the Protection cancels the Sabotage
+		"""
+
+		begin_assets = self.so.sabotaged_corporation.assets
+		truc = self.so.sabotaged_corporation.protecteurs.all()
+		print "Truc: "
+		print truc
+
+		self.po.resolve_successful()
+		self.so.resolve_successful()
+		self.assertEqual(self.reload(self.so.sabotaged_corporation).assets, begin_assets)
+
+	def test_so_po_so(self):
+		"""
+		Test that the Protection only cancels one Sabotage
+		"""
+
+		begin_assets = self.so.sabotaged_corporation.assets
+
+		self.po.resolve_successful()
+		self.so.resolve_successful()
+		self.so.clean()
+		self.so.resolve_successful()
+		self.assertEqual(self.reload(self.so.sabotaged_corporation).assets, begin_assets - 2)
+
+	def test_dso_po_dso_dso(self):
+		"""
+		In that case, the first DataSteal fails because of the Protection, so the second
+		should succeed and benefit the client while the third succeeds without benefits
+		"""
+
+		self.bc3 = BaseCorporation(name="Hero", description="Kaamoulox")
+		self.bc3.save()
+		
+		self.c3 = Corporation(game=self.g, base_corporation=self.bc3)
+		self.c3.assets = 20
+		self.c3.protected = False
+		self.c3.save()
+
+		self.dso2 = DataStealOrder(
+			player=self.p,
+			stealer_corporation=self.c3,
+			stolen_corporation=self.c
+		)
+		self.dso2.save()
+
+		begin_assets_stealer1 = self.dso.stealer_corporation.assets
+		begin_assets_stealer2 = self.dso2.stealer_corporation.assets
+		begin_assets_stolen = self.dso.stolen_corporation.assets
+
+		self.po.resolve_successful()
+		self.dso.resolve_successful() #it says successful, but the protection is here
+		self.dso2.resolve_successful()
+		self.dso.clean()
+		self.dso.resolve_successful()
+
+		self.assertEqual(self.reload(self.dso.stealer_corporation).assets, begin_assets_stealer1)
+		self.assertEqual(self.reload(self.dso2.stealer_corporation).assets, begin_assets_stealer2 + 1)
