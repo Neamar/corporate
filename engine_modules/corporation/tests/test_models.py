9d294d36954655b215717ee5e07f2953be44e02c
Neamar <neamar@neamar.fr>
2014-01-26 15:21:22 +0100

    Better constraint handling

--- engine_modules/corporation/tests/test_models.py
+++ engine_modules/corporation/tests/test_models.py
@@ -13,15 +13,28 @@ class ModelTest(EngineTestCase):
 		"""
 		Corporation should have been created alongside the game
 		"""
+
 		corporations = Corporation.objects.all()
 		self.assertEqual(len(corporations), 1)
 		self.assertEqual(corporations[0].base_corporation, self.bc)
 		self.assertEqual(corporations[0].assets, self.bc.initials_assets)
 
+	def test_corporation_deleted_when_asset_drops_to_zero(self):
+		"""
+		Corporation should crash when their assets are null
+		"""
+		
+		c = Corporation.objects.all()[0]
+		c.assets = 0
+		c.save()
+
+		self.assertRaises(Corporation.DoesNotExist, lambda: self.reload(c))
+
 	def test_corporation_deleted_when_asset_drops_below_zero(self):
 		"""
-		Corporation should crash when their assets drops below 0
+		Corporation should crash when their assets drops below 0, without raising IntegrityError.
 		"""
+
 		c = Corporation.objects.all()[0]
 		c.assets = -1
 		c.save()
