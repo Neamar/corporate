7b99fb544cea0f8c0ccb38fe66079e925063f24d
Neamar <neamar@neamar.fr>
2014-01-26 19:42:17 +0100

    First draft

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
-from website.views import index
+from website import views
 
 urlpatterns = patterns('',
-	url(r'^$', index),
+	url(r'^$', views.index),
 	url(r'^login/$', 'django.contrib.auth.views.login'),
+	url(r'^game/(?P<game_id>[0-9]+)/orders$', views.orders),
 )
--- website/views.py
+++ website/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from engine.models import Game
 
 
 def index(request):
 	return render(request, 'index.html', {})
+
+
+def get_game(request, game_id):
+	"""
+	Retrieve a game.
+	Returns 404 exception if the user has no access to this game, or the game does not exists.
+	"""
+	return get_object_or_404(Game, pk=game_id, player_set__user=request.user)
+
+
+@login_required
+def orders(request, game_id):
+	return get_game(request, game_id)
