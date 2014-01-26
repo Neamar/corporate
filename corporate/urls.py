061954bb21ffc8684731ca35f391633f9142bbec
Neamar <neamar@neamar.fr>
2014-01-26 16:09:28 +0100

    Removed dead code

--- corporate/urls.py
+++ corporate/urls.py
@@ -7,6 +7,5 @@ urlpatterns = patterns('',
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
 	url(r'docs/', include('docs.urls')),
-	url(r'game/', include('engine.urls')),
 	url(r'', include('website.urls')),
 	)
--- engine/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, url
-from engine import views
-
-urlpatterns = patterns('',
-	url(r'(?P<game_id>[0-9]+)$', views.index),
-	url(r'^(?P<game_id>[0-9]+)/players$', views.players),
-)
