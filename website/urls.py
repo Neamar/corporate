1aa078ea58ada8574495f75f475dbfc800417235
Neamar <neamar@neamar.fr>
2014-01-27 16:56:53 +0100

    Favicon

--- website/urls.py
+++ website/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url
 from website.views import index
 
 urlpatterns = patterns('',
+	(r'^favicon.ico$', 'django.views.static.serve', {'document_root': '/path/to/favicon'}),
 	url(r'^$', index),
 	url(r'^login/$', 'django.contrib.auth.views.login'),
 )
