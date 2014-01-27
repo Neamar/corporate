533933ed984007e6564290b1df0e8558979cc282
Neamar <neamar@neamar.fr>
2014-01-27 17:01:12 +0100

    Admin for runs

--- engine_modules/corporation_run/admin.py
+++ engine_modules/corporation_run/admin.py
@@ -3,14 +3,15 @@ from django.contrib import admin
 from engine_modules.corporation_run.models import DataStealOrder, ProtectionOrder, SabotageOrder
 
 class DataStealOrderAdmin(admin.ModelAdmin):
-	list_display = ('stealer_corporation', 'stolen_corporation')
+	list_display = ('pk', 'has_succeeded', 'target_corporation', 'stealer_corporation')
 admin.site.register(DataStealOrder, DataStealOrderAdmin)
 
 
 class ProtectionOrderAdmin(admin.ModelAdmin):
-	list_display = ('protected_corporation', )
+	list_display = ('protected_corporation', 'done')
 admin.site.register(ProtectionOrder, ProtectionOrderAdmin)
 
+
 class SabotageOrderAdmin(admin.ModelAdmin):
-	list_display = ('target_corporation', )
+	list_display = ('pk', 'target_corporation')
 admin.site.register(SabotageOrder, SabotageOrderAdmin)
