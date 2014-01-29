2f6dea220119e992331e5e331031a3e8ce3676a1
Hugo Duroux <hugoduroux@free.fr>
2014-01-30 00:34:58 +0100

    add DerivativeSpeculationOrder

--- engine_modules/speculation/models.py
+++ engine_modules/speculation/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from engine.models import Order
 from engine_modules.corporation.models import Corporation
+from engine_modules.corporation_asset_history.models import AssetHistory
 
 
 class CorporationSpeculationOrder(Order):
@@ -32,22 +33,64 @@ class CorporationSpeculationOrder(Order):
 				# Speculation on first or last corpo
 				self.player.money += self.get_cost() * 2
 				self.player.save()
-				content = u"Vous êtes un bon spéculateur, vos investissements vous ont rapporté %s" % self.investment * 3
+				content = u"Vous êtes un bon spéculateur, vos investissements sur la corporation %s vous ont rapporté %s" % (self.corporation.base_corporation.name, self.investment * 3)
 			else:
 				# Speculation on non first or non last corpo
 				self.player.money += self.get_cost() * 4
 				self.player.save()
-				content = u"Vous êtes un excellent spéculateur, vos investissements vous ont rapporté %s" % self.investment * 5
+				content = u"Vous êtes un excellent spéculateur, vos investissements sur la corporation %s vous ont rapporté %s" % (self.corporation.base_corporation.name, self.investment * 5)
 		else:
 			# Failure
 			self.player.money -= self.get_cost()
 			self.player.save()
-			content = u"Vos spéculations n'ont malheureusement pas étés concluantes"
+			content = u"Vos spéculations sur la corporation %s n'ont malheureusement pas été concluantes" % self.corporation.base_corporation.name
 
 		self.player.add_note(category=category, content=content)
 
 	def description(self):
 		return u"Miser %s ny sur la postion %s de la corporation %s" % (self.get_cost(), self.rank, self.corporation.base_corporation.name)
+
+
+class DerivativeSpeculationOrder(Order):
+	"""
+	Order to speculate on a derivative up or down
+	"""
+	BASE_COST = 10
+
+	UP = True
+	DOWN = False
+
+	UPDOWN_CHOICES = (
+		(UP, 'up'),
+		(DOWN, 'down')
+	)
+
+	speculation = models.BooleanField(choices=UPDOWN_CHOICES)
+	derivative = models.OneToManyField(Corporation)
+	investment = models.PositiveIntegerField()
+
+	def get_cost(self):
+		return self.investment * self.BASE_COST
+
+	def resolve(self):
+
+		# Build message
+		category = u"Spéculations"
+		current_turn_sum = AssetHistory.objects.filter(corporation__in=self.derivative.all(), turn=self.player.game.current_turn).sum()
+		previous_turn_sum = AssetHistory.objects.filter(corporation__in=self.derivative.all(), turn=self.player.game.current_turn - 1).sum()
+
+		if current_turn_sum > previous_turn_sum and self.speculation == self.UP:
+			# Success
+			self.player.money += self.get_cost() * 2
+			self.player.save()
+			content = u"Vous êtes un bon spéculateur, vos investissements sur les produits dérivés vous ont rapporté %s" % self.investment * 2
+		else:
+			# Failure
+			self.player.money -= self.get_cost()
+			self.player.save()
+			content = u"Vos spéculations sur les produits dérivés n'ont malheureusement pas étés concluantes"
+
+		self.player.add_note(category=category, content=content)
 
 
 orders = (CorporationSpeculationOrder,)
--- engine_modules/speculation/tasks.py
+++ engine_modules/speculation/tasks.py
@@ -1,5 +1,5 @@
 from engine.tasks import OrderResolutionTask
-from engine_modules.speculation.models import CorporationSpeculationOrder
+from engine_modules.speculation.models import CorporationSpeculationOrder, DerivativeSpeculationOrder
 
 
 class CorporationSpeculationTask(OrderResolutionTask):
@@ -9,5 +9,12 @@ class CorporationSpeculationTask(OrderResolutionTask):
 	RESOLUTION_ORDER = 900
 	ORDER_TYPE = CorporationSpeculationOrder
 
+class DerivativeSpeculationTask(OrderResolutionTask):
+	"""
+	Speculate on a corporation's rank
+	"""
+	RESOLUTION_ORDER = 900
+	ORDER_TYPE = DerivativeSpeculationOrder
 
-tasks = (CorporationSpeculationTask,)
+
+tasks = (CorporationSpeculationTask, DerivativeSpeculationTask)
