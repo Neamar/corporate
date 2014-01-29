e3945d9f061b2df62b50d04771712cf5fb9062d8
Neamar <neamar@neamar.fr>
2014-01-30 00:56:40 +0100

    Testing login required

---
+++ website/tests.py
@@ -0,0 +1,18 @@
+from django.test import Client
+from django.core.urlresolvers import reverse
+
+from engine.testcases import EngineTestCase
+
+class WebsiteTest(EngineTestCase):
+	def setUp(self):
+		self.c = Client()
+		super(WebsiteTest, self).setUp()
+
+class OrderWebsiteTest(WebsiteTest):
+	def test_orders_require_login(self):
+		r = self.c.get(reverse('website.views.orders', args=[self.g.id]))
+		self.assertEqual(r.status_code, 302)
+
+	def test_posting_orders_require_login(self):
+		r = self.c.get(reverse('website.views.add_order', args=[self.g.id, 'BuyInfluenceOrder']))
+		self.assertEqual(r.status_code, 302)
