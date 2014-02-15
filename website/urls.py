e71cbacb32ae8ab5bf89d677c2a6e9e7bc8a0575
Jules <jules.duroux@gmail.com>
2014-02-15 22:16:35 +0100

    second table for players

--- website/urls.py
+++ website/urls.py
@@ -8,5 +8,7 @@ urlpatterns = patterns('',
 	url(r'^game/(?P<game_id>[0-9]+)/orders/post/(?P<order_type>\w+)$', views.add_order),
 	url(r'^game/(?P<game_id>[0-9]+)/wallstreet$', views.wallstreet),
 	url(r'^game/(?P<game_id>[0-9]+)/corporations$', views.corporations),
+	url(r'^game/(?P<game_id>[0-9]+)/corporations/(?P<corporation_id>[0-9])$', views.corporation),
 	url(r'^game/(?P<game_id>[0-9]+)/players$', views.players),
+	url(r'^game/(?P<game_id>[0-9]+)/players/(?P<player_id>[0-9])$', views.player),
 )
