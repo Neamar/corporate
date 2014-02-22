16a34c0288369503ff1c8fd9e00978931833b731
Neamar <neamar@neamar.fr>
2014-02-22 17:17:31 +0100

    Fixed admin

--- engine_modules/player_run/admin.py
+++ engine_modules/player_run/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from engine_modules.player_run.models import InformationRunOrder
+from engine_modules.player_run.models import InformationOrder
 
 # Register your models here.
-admin.site.register(InformationRunOrder)
+admin.site.register(InformationOrder)
