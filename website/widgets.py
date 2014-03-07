6f9fdb57a3298db403087cc3ac4bc7d46f815c24
Neamar <neamar@neamar.fr>
2014-03-07 11:09:26 +0100

    Redisplay initial value for bound forms

--- website/widgets.py
+++ website/widgets.py
@@ -1,14 +1,14 @@
 from django import forms
 
 
-class PlainTextWidget(forms.TextInput):
-	def render(self, name, value, attrs):
-		return "%s" % value
-		return super(PlainTextWidget, self).render(name, value, attrs)
-
-
 class PlainTextField(forms.CharField):
 	def __init__(self, *args, **kwargs):
+		# Closure for initial value (else displaying a bound form show None)
+		class PlainTextWidget(forms.TextInput):
+			def render(self, name, value, attrs):
+				return kwargs['initial']
+				return super(PlainTextWidget, self).render(name, value, attrs)
+
 		kwargs.setdefault('widget', PlainTextWidget)
 		kwargs.setdefault('required', False)
 		super(PlainTextField, self).__init__(*args, **kwargs)
