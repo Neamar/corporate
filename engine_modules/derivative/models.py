e4bc1d41802b6ec59db33a77ec16963579af2a95
Neamar <neamar@neamar.fr>
2014-03-31 15:59:07 +0200

    Derivative model

--- engine_modules/derivative/models.py
+++ engine_modules/derivative/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from engine_modules.corporation.models import Corporation
+from engine_modules.corporation_asset_history.models import AssetHistory
+from engine.models import Game
+from django.db.models import Sum
 
-# Create your models here.
+
+class Derivative(models.Model):
+	name = models.CharField(max_length=30)
+	game = models.ForeignKey(Game)
+	corporations = models.ManyToManyField(Corporation)
+
+	def __unicode__(self):
+		return self.name
+
+	def get_sum(self, turn):
+		return AssetHistory.objects.filter(corporation__in=self.corporations.all(), turn=turn).aggregate(Sum('assets'))['assets__sum']
