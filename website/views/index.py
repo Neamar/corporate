d63665597493bc8f59016c113b7ca5bec3257c76
Neamar <neamar@neamar.fr>
2014-04-06 12:56:51 +0200

    Restored index

--- website/views/index.py
+++ website/views/index.py
@@ -1,9 +1,19 @@
 from __future__ import absolute_import
-from django.shortcuts import render
+
+from website.decorators import render
+from website.models import User
 
 
+@render('index.html')
 def index(request):
 	"""
 	Welcome to the Corporate Game!
 	"""
-	return render(request, 'index.html', {})
+
+	players = []
+	if type(request.user) == User:
+		players = request.user.player_set.all().select_related('game')
+
+	return {
+		"players": players
+	}
