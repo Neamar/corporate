08539ac8e42df857e00fc6a2912fd655bb0122ca
Neamar <neamar@neamar.fr>
2014-04-12 20:32:09 +0200

    Forbid Datasteal and Extraction (signals)

---
+++ engine_modules/corporation_run/signals.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from django.dispatch import receiver
+
+from engine.dispatchs import validate_order
+from engine.exceptions import OrderNotAvailable
+
+from engine_modules.corporation_run.models import DataStealOrder, ExtractionOrder
+
+
+@receiver(validate_order, sender=DataStealOrder)
+def datasteal_target_stealer_differ(sender, instance, **kwargs):
+	"""
+	Datasteal target and stealer must be different
+	"""
+	if instance.target_corporation == instance.stealer_corporation:
+		raise OrderNotAvailable("La cible et le bénéficiaire doivent être différents !")
+
+
+@receiver(validate_order, sender=ExtractionOrder)
+def extraction_target_stealer_differ(sender, instance, **kwargs):
+	"""
+	Extraction target and kidnapper must be different
+	"""
+	if instance.target_corporation == instance.kidnapper_corporation:
+		raise OrderNotAvailable("La cible et le bénéficiaire doivent être différents !")
