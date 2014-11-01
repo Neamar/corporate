6bf0badaf753ce37cae7f849de3382d5602553d2
Neamar <neamar@neamar.fr>
2014-11-01 15:35:36 +0100

    Updated decorator

--- engine/decorators.py
+++ engine/decorators.py
@@ -1,15 +1,19 @@
+from functools import wraps
+
+
 def sender_instance_of(*args):
 	"""
 	Only call decorated function if sender is an instance of one of the specified class.
 	"""
 	Orders = args
 
-	def d(func):
-		def wrapper(*args, **kwargs):
+	def decorator(func):
+		@wraps(func)
+		def wrap(*args, **kwargs):
 			# Only validate for Offensive runs.
 			for Order in Orders:
 				if isinstance(kwargs['instance'], Order):
 					func(*args, **kwargs)
 					return
-		return wrapper
-	return d
+		return wrap
+	return decorator
