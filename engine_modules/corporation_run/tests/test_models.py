ff0da81e484c12d918f204b100be11324dc90d62
Neamar <neamar@neamar.fr>
2015-02-22 17:29:38 +0100

    Test for recipient_target property

--- engine_modules/corporation_run/tests/test_models.py
+++ engine_modules/corporation_run/tests/test_models.py
@@ -1,7 +1,7 @@
 from django.core.exceptions import ValidationError
 
 from engine.testcases import EngineTestCase
-from engine_modules.corporation_run.models import DataStealOrder, ExtractionOrder
+from engine_modules.corporation_run.models import DataStealOrder, ProtectionOrder
 
 
 class ModelsTest(EngineTestCase):
@@ -14,13 +14,6 @@ class ModelsTest(EngineTestCase):
 		)
 		self.dso.save()
 
-		self.eo = ExtractionOrder(
-			player=self.p,
-			target_corporation_market=self.c.historic_corporation_market,
-			stealer_corporation=self.c2,
-		)
-		self.eo.save()
-
 	def test_target_corporation_property(self):
 		"""
 		target_corporation must return the right corporation
@@ -32,3 +25,16 @@ class ModelsTest(EngineTestCase):
 		stealer_corporation_market must return the right corporation_market
 		"""
 		self.assertEqual(self.dso.stealer_corporation_market, self.c2.corporationmarket_set.get(market=self.dso.target_corporation_market.market))
+
+	def test_protected_corporation_property(self):
+		"""
+		protected_corporation must return the right corporation
+		"""
+
+		po = ProtectionOrder(
+			player=self.p,
+			protected_corporation_market=self.c.historic_corporation_market,
+		)
+		po.save()
+
+		self.assertEqual(po.protected_corporation, self.c)
