5a92a7add65213699815716806413d9ea57f10c1
Neamar <neamar@neamar.fr>
2015-03-19 16:09:04 +0100

    Added production environment

--- corporate/settings.py
+++ corporate/settings.py
@@ -115,3 +115,24 @@ else:
     CITY = "Manhattan"
 
 CITY_BASE_DIR = "%s/data/cities/%s" % (BASE_DIR, CITY.lower())
+
+
+# Environment overrides
+if "PYTHON_ENV" in os.environ and os.environ["PYTHON_ENV"] == "production":
+    import dj_database_url
+    DATABASES['default'] = dj_database_url.config()
+
+    # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+    # Allow all host headers
+    ALLOWED_HOSTS = ['*']
+
+    # Static asset configuration
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    STATIC_ROOT = 'staticfiles'
+    STATIC_URL = '/static/'
+
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
