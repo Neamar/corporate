d5c5dad03f7a31d17d4435332f10721a1ef50349
Neamar <neamar@neamar.fr>
2015-08-01 23:51:51 +0200

    Fix indexing

--- website/management/commands/stubgame.py
+++ website/management/commands/stubgame.py
@@ -6,10 +6,10 @@ from engine.models import Game, Player
 class Command(BaseCommand):
 	help = 'Create a stub game for testing'
 
-	def create_user(self, password, **kwargs):
+	def create_user(self, username, **kwargs):
 		User = get_user_model()
-		user, created = User.objects.get_or_create(**kwargs)
-		user.set_password(password)
+		user, created = User.objects.get_or_create(username=username, **kwargs)
+		user.set_password(username)
 		user.save()
 		return user
 
@@ -21,21 +21,21 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		# Create admin user
 		self.stdout.write("Creating admin...")
-		self.create_user(password="admin", username="admin", is_staff=True, is_superuser=True)
+		self.create_user(username="admin", is_staff=True, is_superuser=True)
 
 		# Create other users
 		self.stdout.write("Creating users...")
-		user1 = self.create_user(password="user", username="player1")
-		user2 = self.create_user(password="user", username="player2")
-		user3 = self.create_user(password="user", username="player3")
-		user4 = self.create_user(password="user", username="player4")
+		user1 = self.create_user(username="player1")
+		user2 = self.create_user(username="player2")
+		user3 = self.create_user(username="player3")
+		user4 = self.create_user(username="player4")
 
 		# Create game
 		self.stdout.write("Creating stub game...")
 		self.g = Game.objects.create(city="Stub")
 		self.p1 = self.create_player(game=self.g, user=user1, name="player1", background="Player 1 background")
-		self.p2 = self.create_player(game=self.g, user=user2, name="player1", background="Player 2 background")
-		self.p3 = self.create_player(game=self.g, user=user3, name="player1", background="Player 3 background")
-		self.p4 = self.create_player(game=self.g, user=user4, name="player1", background="Player 4 background")
+		self.p2 = self.create_player(game=self.g, user=user2, name="player2", background="Player 2 background")
+		self.p3 = self.create_player(game=self.g, user=user3, name="player3", background="Player 3 background")
+		self.p4 = self.create_player(game=self.g, user=user4, name="player4", background="Player 4 background")
 
-		self.stdout.write("Created game #%s, connect with username `player1` and password `user`" % self.g.pk)
+		self.stdout.write("Created game #%s, connect with username `player1` and password `player1`" % self.g.pk)
