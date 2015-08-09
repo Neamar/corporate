7a27c6d6f6267b78ea3e7ecd453efc7babe8a3d4
Neamar <neamar@neamar.fr>
2015-08-09 16:01:18 +0200

    Added back import

--- corporate/wsgi.py
+++ corporate/wsgi.py
@@ -8,8 +8,9 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "corporate.settings")
+
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "corporate.settings")
 application = Cling(get_wsgi_application())
