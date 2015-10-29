749762d3254a4e6e330e4344e66f7982e996685a
your-mom-noob <bom@neamar.fr>
2015-10-29 14:59:16 +0100

    Added an 'AssetDelta' category for market bubbles

--- engine_modules/market/models.py
+++ engine_modules/market/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 
 from engine.models import Game
-
+from engine_modules.corporation.models import AssetDelta
 
 class Market(models.Model):
 	class Meta:
@@ -62,7 +62,10 @@ class CorporationMarket(models.Model):
 			self.value -= 1
 			self.bubble_value -= 1
 		self.save()
-		return self.bubble_value - previous_bubble_value
+
+		delta = self.bubble_value - previous_bubble_value
+		self.corporation.assetdelta_set.create(category=AssetDelta.BUBBLE, delta=delta, turn=self.turn)
+		return delta
 
 	def __unicode__(self):
 		return u"%s de %s" % (self.market, self.corporation)
