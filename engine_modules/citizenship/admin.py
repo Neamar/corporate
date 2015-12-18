4dd3aa52a3ef5581311b491e1f188be86d44c509
Jules <jules.duroux@gmail.com>
2015-12-18 10:25:29 +0100

    pump citizenship admin

--- engine_modules/citizenship/admin.py
+++ engine_modules/citizenship/admin.py
@@ -3,5 +3,14 @@ from django.contrib import admin
 from engine_modules.citizenship.models import Citizenship, CitizenshipOrder
 
 
-admin.site.register(Citizenship)
+class CitizenshipAdmin(admin.ModelAdmin):
+	list_display = ('player', 'corporation', 'turn', 'game')
+	list_filter = ('game',)
+
+	def corporation(self, instance):
+		return self.corporation.base_corporation.name
+
+	def game(self, instance):
+		return self.player.game
+admin.site.register(Citizenship, CitizenshipAdmin)
 admin.site.register(CitizenshipOrder)
