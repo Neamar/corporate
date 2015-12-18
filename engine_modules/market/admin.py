61e6deefb454a7269b196eda16dc7d64fb05c0ce
Jules <jules.duroux@gmail.com>
2015-12-18 10:30:22 +0100

    pump corporationMarket admin

--- engine_modules/market/admin.py
+++ engine_modules/market/admin.py
@@ -9,6 +9,10 @@ admin.site.register(Market, MarketAdmin)
 
 
 class CorporationMarketAdmin(admin.ModelAdmin):
-	list_display = ('corporation', 'market', 'turn', 'value')
+	list_display = ('game', 'corporation', 'market', 'turn', 'value')
+	list_filter = ('game', 'corporation', 'market', 'turn', )
+
+	def game(self, instance):
+		return self.corporation.game
 
 admin.site.register(CorporationMarket, CorporationMarketAdmin)
