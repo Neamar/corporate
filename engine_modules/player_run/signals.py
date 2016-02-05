0971b30997c79dc0d1b7c2cb69260c3bef1c6080
Jules <jules.duroux@gmail.com>
2016-02-05 13:43:14 +0100

    good price for information run v10 ><

--- engine_modules/player_run/signals.py
+++ engine_modules/player_run/signals.py
@@ -14,13 +14,13 @@ def information_m2m_changed(action, instance, model, **kwargs):
 		price_all_orders = Order.objects.filter(player=instance.player, turn=instance.player.game.current_turn).annotate(total_price=Sum('cost'))[0].total_price
 		if model == Player:
 			if price_all_orders + instance.cost + InformationOrder.PLAYER_COST > instance.player.money:
-				instance.remove(model)
+				instance.player_targets.remove(model)
 			else:
 				instance.cost += InformationOrder.PLAYER_COST
 				instance.save()
 		elif model == Corporation:
 			if price_all_orders + instance.cost + InformationOrder.CORPORATION_COST > instance.player.money:
-				instance.remove(model)
+				instance.corporation_targets.remove(model)
 			else:
 				instance.cost += InformationOrder.CORPORATION_COST
 				instance.save()
