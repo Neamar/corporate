151e56a8e1bbe1b4676860a1a74bd10ff7b758b2
Jules <jules.duroux@gmail.com>
2016-02-06 23:02:03 +0100

    filter corporation for changing nationnalities

--- data/order_description/InformationOrder.md
+++ data/order_description/InformationOrder.md
@@ -1,3 +1,4 @@
 Une opération d'information a 100% de chances de réussite et vous donnera des informations en fonction des cibles :
+
 * sur un joueur : tous les ordres qu'il a passé ce tour et son information de background
 * sur une corpo : tous les effets qui ont visé l'un des marchés de la corporation.
--- engine_modules/citizenship/models.py
+++ engine_modules/citizenship/models.py
@@ -40,7 +40,8 @@ class CitizenshipOrder(Order):
 
 	def get_form(self, data=None):
 		form = super(CitizenshipOrder, self).get_form(data)
-		form.fields['corporation'].queryset = self.player.game.corporation_set.all()
+		inner_qs = self.player.share_set.all().values("corporation__pk")
+		form.fields['corporation'].queryset = self.player.game.corporation_set.filter(corporation__pk=inner_qs)
 		return form
 
 orders = (CitizenshipOrder,)
