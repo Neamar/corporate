dfe3fbb5658a3af8bc95b2b66aa1e1e70313a186
Matthieu Bacconnier <neamar@neamar.fr>
2016-03-04 21:23:52 +0000

    Enable debug toolbar

--- corporate/settings.py
+++ corporate/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
-    # 'debug_toolbar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admindocs',
+    'debug_toolbar',
     'compressor',
     'website',
     'docs',
