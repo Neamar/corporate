5f876fddd2737a462fcac89867d81ef1dd60ac7e
Jules <jules.duroux@gmail.com>
2016-03-27 11:58:09 +0200

    titles in wallstreet v4

--- website/templatetags/display_log.py
+++ website/templatetags/display_log.py
@@ -12,7 +12,7 @@ def display_log(log, display_context, delta_display=True, size="medium", color="
 	The criterion here is whether the URL contains the keyword, not necessarily very robust.
 	"""
 
-	r = """<span title="%s"><svg role="img" class="svg--%s svg--%s"><use xlink:href="/static/img/sprite.svg#%s"></use></svg></span>""" % (escape(log.get_display(display_context)), size, color, log.event_type.lower())
+	r = """<span title="%s"><svg role="img" class="svg--%s svg--%s"><use xlink:href="/static/img/sprite.svg#%s"></use></svg></span>""" % (log.get_display(display_context), size, color, log.event_type.lower())
 
 	if delta_display and log.delta != 0:
 		delta_type = ""
