5dba5831564ee53cca902c90a56b33865659a220
Jules <jules.duroux@gmail.com>
2016-11-13 19:05:30 +0100

    bug fix

--- engine/models.py
+++ engine/models.py
@@ -196,7 +196,7 @@ class Game(models.Model):
 		from logs.models import Log
 		from engine_modules.detroit_inc.models import DIncVoteOrder
 
-		votes = Log.objects.filter(concernedplayer__player=player, concernedplayer__personal=True, turn__lte=turn, game=player.Game).filter(Q(event_type=Game.VOTE_SECURITY) | Q(event_type=Game.VOTE_CONSOLIDATION) | Q(event_type=Game.VOTE_CONTRAT))
+		votes = Log.objects.filter(concernedplayer__player=player, concernedplayer__personal=True, turn__lte=turn, game=player.game).filter(Q(event_type=Game.VOTE_SECURITY) | Q(event_type=Game.VOTE_CONSOLIDATION) | Q(event_type=Game.VOTE_CONTRAT))
 		for vote in votes:
 			if player.game.get_dinc_coalition(turn=vote.turn) == DIncVoteOrder.CONS and vote.event_type == Game.VOTE_CONSOLIDATION:
 				points += 3
