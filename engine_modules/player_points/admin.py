90d7485cf34e1b6bd3abcbfe4ac78ce3936a36b2
Jules <jules.duroux@gmail.com>
2016-11-13 19:31:25 +0100

    admin bub fix

--- engine_modules/player_points/admin.py
+++ engine_modules/player_points/admin.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-
 from engine_modules.player_points.models import PlayerPoints
 
 
-class PlayerPontsAdmin(admin.ModelAdmin):
-	inlines = [PlayerPoints]
+class PlayerPointsAdmin(admin.ModelAdmin):
 	list_display = ('player', 'total_points', 'citizenship_points', 'background_points', 'dinc_points')
 	list_filter = ('player',)
 
 
-admin.site.register(PlayerPoints, PlayerPontsAdmin)
+admin.site.register(PlayerPoints, PlayerPointsAdmin)
--- engine_modules/player_points/models.py
+++ engine_modules/player_points/models.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import models
-
 from engine.models import Player
 
 
