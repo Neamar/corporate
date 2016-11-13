c8257d508f6dc89d89acdc5d7e0657b119d233f5
Jules <jules.duroux@gmail.com>
2016-11-13 19:16:15 +0100

    resgister admin

--- engine_modules/player_points/admin.py
+++ engine_modules/player_points/admin.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 from engine_modules.player_points.models import PlayerPoints
 
 
-class CorporationAdmin(admin.ModelAdmin):
+class PlayerPontsAdmin(admin.ModelAdmin):
 	inlines = [PlayerPoints]
 	list_display = ('player', 'total_points', 'citizenship_points', 'background_points', 'dinc_points')
 	list_filter = ('player',)
+
+
+admin.site.register(PlayerPontsAdmin)
