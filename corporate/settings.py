43a6a422f738913b26d13cc56085e336225033f6
Jules <email@example.com>
2019-02-27 15:18:17 +0100

    missing conf for whitenoise ?

--- corporate/settings.py
+++ corporate/settings.py
@@ -82,6 +82,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 )
 
 ROOT_URLCONF = 'corporate.urls'
